8f43f79ebab51fcd677341bdbcbf70d926b4495a
WangHaoRan <whrlmc@139.com>
2013-12-20 01:31:01 +0800

    测试授权流程

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -30,13 +30,25 @@ class IndexAction extends Action {
     }
 
     public function info(){
-        /*
+        $this -> display();
+
+
+    }
+
+    public function result(){
         if(!empty($_GET['id'])){
             $info = R('Type/getType', array($_GET['id']), 'Widget');
             dump($info);
         }
-        */
-        echo 'Hello!';
+        include_once('./saetv2.ex.class.php');
+        $c = new SaeTClientV2(C('WB_AKEY'), C('WB_SKEY'), $_SESSION['token']['access_token']);
+        $uid_get = $c->get_uid();
+        dump($uid_get);
+        $uid = $uid_get['uid'];
+        //get Friend Ids
+        $temp = $c-> bilateral_ids( $uid);
+        dump($temp);
+
     }
 
 }
