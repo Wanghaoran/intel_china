611184918ff256d45df1d137067aa3099fcdbc0e
WangHaoRan <whrlmc@139.com>
2013-12-22 19:17:41 +0800

    测试记录信息

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -38,14 +38,21 @@ class IndexAction extends Action {
     }
 
     public function recordnew(){
-        dump($_POST);
+        $User = M('User');
+        $data = array();
+        $data['uid'] = $_POST['uid'];
+        $data['join_time'] = time();
+        $data['type'] = $_POST['type'];
+        $data['type_name'] = $_POST['type_name'];
+        $data['idstr'] = $_POST['idstr'];
+        if($User -> add($data)){
+            $this -> success('success');
+        }else{
+            $this -> error('error');
+        }
     }
 
     public function result(){
-        if(!empty($_GET['id'])){
-            $info = R('Type/getType', array($_GET['id']), 'Widget');
-            dump($info);
-        }
         include_once('./saetv2.ex.class.php');
         $c = new SaeTClientV2(C('WB_AKEY'), C('WB_SKEY'), $_SESSION['token']['access_token']);
         $uid_get = $c->get_uid();
@@ -54,6 +61,9 @@ class IndexAction extends Action {
         //get Friend Ids
         $temp = $c-> bilateral_ids( $uid);
         dump($temp);
+        $User = M('User');
+        $result = $User -> find($uid);
+        dump($result);
 
     }
 
